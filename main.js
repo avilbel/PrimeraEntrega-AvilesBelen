c154e55b5cec61332e7648ed4e0978d868880e89
avilbel <131410638+avilbel@users.noreply.github.com>
2023-06-05 20:33:42 -0300

    .

--- main.js
+++ main.js
@@ -15,35 +15,29 @@ console.log(sayHi(userName));
 
 
 function menu() {
-    prompt(userName + 'Choose one option from the following: \n 1) Paper \n 2) Rock \n 3)Scissors ');
+    prompt(userName + ', choose one option from the following: \n 1) Paper \n 2) Rock \n 3)Scissors ');
 }
 
 userAnswer = menu();
 // userAnswer();
 console.log(userAnswer);
-// computerAnswer = "1";
 
-// console.log(result(userAnswer, "1"));
 let round1A;
 
 round1A = result(userAnswer, "2");
 
 function result(userAnswer, computerAnswer) {
     if (((userAnswer == "1") && (computerAnswer == "1")) || ((userAnswer == "2") && (computerAnswer == "2")) || ((userAnswer == "3") && (computerAnswer == "3"))) {
-        alert("DRAW");
+        return alert("DRAW");
     } else {
         if (((userAnswer == "1") && (computerAnswer == "2")) || ((userAnswer == "2") && (computerAnswer == "3")) || ((userAnswer == "3") && (computerAnswer == "1"))) {
-            alert(userName + "Wins!!");
+            return alert(userName + "Wins!!");
         } else {
             if (((userAnswer == "1") && (computerAnswer == "3")) || ((userAnswer == "2") && (computerAnswer == "1")) || ((userAnswer == "3") && (computerAnswer == "2"))) {
-                alert("Computer Wins!!");
+                return alert("Computer Wins!!");
             }
         }
     }
+    return "Error";
 }
 
-// computerAnswer = "1";
-
-// result(userAnswer, "3");
-
-// round1A = result(userAnswer, "2");
